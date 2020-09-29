afeb511fe7beb4356f80ab38bec0fe8042bf5b7b
zhang <15878000409@163.com>
2020-09-29 17:11:28 +0800

    fix SSR

--- linux下SSR的搭建.c
+++ linux下SSR的搭建.c
@@ -1,16 +1,17 @@
 1.SSR的安装：
 yum -y install wget 
-wget -N --no-check-certificate https://softs.fun/Bash/ssr.sh && chmod +x ssr.sh && bash ssr.sh
+wget -N --no-check-certificate https://softs.fun/Bash/ssr.sh
+chmod +x ssr.s
+bash ssr.sh
 
-备用脚本1：
-yum -y install wget 
-wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh && bash ssr.sh
 
-备用脚本2
+备用脚本:
 yum -y install wget 
-wget -N --no-check-certificate https://cdn.qqiyu.cn/ssr.sh && chmod +x ssr.sh && bash ssr.sh
+wget -N --no-check-certificate https://cdn.qqiyu.cn/ssr.sh
 
 
 2.BBR安装：
 yum -y install wget 
-wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh chmod +x bbr.sh ./bbr.sh
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh 
+chmod +x bbr.sh
+./bbr.sh
