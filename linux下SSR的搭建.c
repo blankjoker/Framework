e8a4aa0c42e5cdbea3784b1d53c7fd9dd3d2b5fa
zhang <15878000409@163.com>
2020-09-29 17:04:05 +0800

    add SSR framework

---
+++ linux下SSR的搭建.c
@@ -0,0 +1,16 @@
+1.SSR的安装：
+yum -y install wget 
+wget -N --no-check-certificate https://softs.fun/Bash/ssr.sh && chmod +x ssr.sh && bash ssr.sh
+
+备用脚本1：
+yum -y install wget 
+wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh && bash ssr.sh
+
+备用脚本2
+yum -y install wget 
+wget -N --no-check-certificate https://cdn.qqiyu.cn/ssr.sh && chmod +x ssr.sh && bash ssr.sh
+
+
+2.BBR安装：
+yum -y install wget 
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh chmod +x bbr.sh ./bbr.sh
